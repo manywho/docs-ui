969daa8b8b6d3427e999bfae17c8f89e5456f8bb
Dale Shoulders <dshoulders@outlook.com>
2022-06-07 17:05:51 +0100

    FLOW-5023 - Reduce docs

--- custom-input/custom-input.js
+++ custom-input/custom-input.js
@@ -7,47 +7,45 @@
  * This colour code is then applied to the background colour of the text input making it change colour as the user types.
  */
 
- const { React } = window.boomi.flow;
- 
- /** Generates a numeric value from a string */
- function hashCode(str) {
-     var hash = 0;
-     for (var i = 0; i < str.length; i++) {
-         hash = str.charCodeAt(i) + ((hash << 5) - hash);
-     }
-     return hash;
- }
- 
- /** Converts a numeric value into hexadecimal RGB colour value */
- function intToRGB(i) {
-     var c = (i & 0x00ffffff).toString(16).toUpperCase();
- 
-     return '00000'.substring(0, 6 - c.length) + c;
- }
- 
- // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
- const InputText = ({ element, updateElement, fireEvents }) => {
- 
-     const onInput = ({ target: { value } }) =>
-         updateElement(element.id, {
-             contentValue: value,
-         });
- 
-     const onBlur = () => {
-         fireEvents(element.id);
-     };
- 
-     const colourCode = intToRGB(hashCode(element.contentValue ?? ''));
- 
-     return React.createElement('input', {
-             style: { backgroundColor: `#${colourCode}` },
-             className: 'input',
-             id: element.id,
-             value: element.contentValue ?? '',
-             onInput,
-             onBlur,
-         }) 
+const { React } = window.boomi.flow;
+
+/** Generates a numeric value from a string */
+function hashCode(str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+}
+
+/** Converts a numeric value into hexadecimal RGB colour value */
+function intToRGB(i) {
+    var c = (i & 0x00ffffff).toString(16).toUpperCase();
+
+    return '00000'.substring(0, 6 - c.length) + c;
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const InputText = ({ element, updateElement, fireEvents }) => {
+    const onInput = ({ target: { value } }) =>
+        updateElement(element.id, {
+            contentValue: value,
+        });
+
+    const onBlur = () => {
+        fireEvents(element.id);
     };
- 
- export default InputText;
- 
+
+    const colourCode = intToRGB(hashCode(element.contentValue ?? ''));
+
+    return React.createElement('input', {
+        style: { backgroundColor: `#${colourCode}` },
+        className: 'input',
+        id: element.id,
+        value: element.contentValue ?? '',
+        onInput,
+        onBlur,
+    });
+};
+
+export default InputText;
